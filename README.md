79782e6169da397cc869c5209eac1e5697399b07
Heath Ward <heathw24@outlook.com>
2021-03-25 19:56:20 -0400

    updated readMe

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # React_Portfolio
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow?style=plastic.svg)](https://opensource.org/licenses/MIT)
+
+This application takes the user to display projects and programming skill using React.js to future. The sections are displayed dynamicly using react State. Projects are stored in a file which is read and displayed dynamically. This portfolio is designed to adjust display based on screen size. This portfolio also includes links to these projects as well as the users github, linkedIn, resume and other helpful contact information.
+
+https://heathw24.github.io/React_Portfolio/
+
+Tools Used:
+1. React
+2. Bootstrap
+3. Node.js
+4. JavaScript
+5. HTML
+6. CSS
+
+![React-Portfolio]()
--- reactportfolio/src/components/contact.js
+++ reactportfolio/src/components/contact.js
@@ -3,14 +3,14 @@ import React from "react";
 function Contact() {
 
     return(
-    <div>
+    <div >
         <section className="mainSection">
-         <h2 class="sectionTitle">Contact Me</h2>
+         <h2 className="sectionTitle">Contact Me</h2>
             <ul>
-                <li class="contactMe">Phone: 864-607-6772</li>
+                <li className="contactMe">Phone: 864-607-6772</li>
                  <a href="mailto: heathw2444@yahoo.com"> <li class="contactMe" > Email: Heathw2444@yahoo.com </li></a>
-                <li><a class="contactMe" href="https://github.com/Heathw24">GitHub</a></li>
-                <li><a class="contactMe" href="https://www.linkedin.com/in/heath-ward-029b9518b">LinkedIn</a></li>
+                <li><a className="contactMe" href="https://github.com/Heathw24">GitHub</a></li>
+                <li><a className="contactMe" href="https://www.linkedin.com/in/heath-ward-029b9518b">LinkedIn</a></li>
        </ul>
 
         <br/>
