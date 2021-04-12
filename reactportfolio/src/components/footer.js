6aff90b6d82f59886464396d92037160b44bfbdd
Heath Ward <heathw24@outlook.com>
2021-04-11 21:47:03 -0400

    updated projects

--- reactportfolio/src/components/footer.js
+++ reactportfolio/src/components/footer.js
@@ -6,7 +6,6 @@ function Footer() {
     return(
         <div>
             <footer>
-                <h3>made using React.js</h3>
                 <h4>Contact Me @ HeathW2444@yahoo.com</h4>
             </footer>
         </div>
