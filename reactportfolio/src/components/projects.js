fd5f09bc858a844ea5ee999bd0f83da1ad0871d6
Heath Ward <heathw24@outlook.com>
2021-04-15 18:29:16 -0400

    fixed repository path

--- reactportfolio/src/components/projects.js
+++ reactportfolio/src/components/projects.js
@@ -13,8 +13,9 @@ class Projects extends Component {
              <div>
                 <section className="mainSection">
                 <h2 className="sectionTitle">Projects</h2>
+                <h2>Click on the links below to view the projects live or click the GitHub links to view the repositories!</h2>
                 <div id="projectSec">
-                 {projectsList.map((p) => <ProjectCards key={p.id} name={p.name} tech={p.tech} href={p.href} imagePath={p.imagePath}/>)}
+                 {projectsList.map((p) => <ProjectCards key={p.id} name={p.name} tech={p.tech} href={p.href} imagePath={p.imagePath} repo={p.repo}/>)}
                 </div>
                 </section>
              </div>
